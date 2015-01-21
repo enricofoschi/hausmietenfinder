7114fcc58df641babad48074f5663fa55a34c9a0
Enrico Foschi <foschi.enrico@gmail.com>
2015-01-21 08:48:51 +0000

    Enabling remote socket io connections

--- HausMietenFinder/public/js/init.jsx
+++ HausMietenFinder/public/js/init.jsx
@@ -8,7 +8,7 @@ ReactRouter.run(routes, ReactRouter.HistoryLocation, function (Handler) {
 // Initializing NodeJS Server Connection
 var socketIONode = MainProperties.absolute_url;
 socketIONode = socketIONode.substr(0, socketIONode.lastIndexOf('/')) + ':3000/';
-var socket = io.connect('http://localhost:3000/');
+var socket = io.connect(socketIONode);
 socket.on('connect', function () {
   socket.on('message', function (msg) {
 
